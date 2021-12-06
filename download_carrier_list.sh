bdb1a15d28d441c258feddda822bfab018de6e13
flawedworld <flawedworld@flawed.world>
2021-12-06 16:15:16 +0000

    Use android12-release for carrier list

--- download_carrier_list.sh
+++ download_carrier_list.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 curl -fS \
-  'https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/latest_carrier_id/carrier_list.pb?format=TEXT' | \
+  'https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/android12-release/assets/latest_carrier_id/carrier_list.pb?format=TEXT' | \
   base64 --decode > carrier_list.pb
